9d2a6fb9866c095062e4c3a52284b82f636e203f
blazion09 <muhd09afiq@gmail.com>
2023-12-13 13:57:16 +0800

    chore grid draft complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const size = "20px";
+const size = "1px";
 const containerSize = size * 16;
 
 const container = document.querySelector(".container");
@@ -8,40 +8,22 @@ container.style.flexDirection = "column";
 container.style.width = containerSize;
 container.style.height = containerSize;
 
-const column = () => {
+let gridY;
+
+const test = () => {
+  let gridX = document.createElement("div");
+  container.appendChild(gridX);
+  gridX.style.display = "flex";
   for (let i = 0; i < 16; i++) {
-    let gridX = document.createElement("div");
-    container.appendChild(gridX);
-    let gridY = document.createElement("div");
+    gridY = document.createElement("div");
     gridX.appendChild(gridY);
-    // gridX.style.borderStyle = "solid";
-    // gridX.style.width = "10px";
-    // gridX.style.width = size;
-    // gridX.style.height = size;
-    gridX.style.display = 'flex';
     gridY.style.borderStyle = "solid";
-    gridY.style.width = "10px";
-    gridY.style.width = size;
-    gridY.style.height = size;
+    gridY.style.width = "20px";
+    gridY.style.height = "20px";
+    gridY.style.borderWidth = "1px";
   }
 };
 
-const row = () => {
-  for (let i = 0; i < 16; i++) {
-    let gridX = document.createElement("div");
-    container.appendChild(gridX);
-    gridX.style.borderStyle = "solid";
-    gridX.style.width = "10px";
-    // gridX.textContent = ".";
-    gridX.style.width = size;
-    gridX.style.height = size;
-  }
-};
-
-// const box = () => {
-//   for (let i = 0; i < 16; i++) {
-//     column();
-//   }
-// };
-
-column();
+for (let j = 0; j < 16; j++) {
+  test();
+}
